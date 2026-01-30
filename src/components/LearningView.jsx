362949f66a8a0e2a4a42beaf2e53d5b4bbf0b9c3
Ankit singh <162681541+ankitsingh1104@users.noreply.github.com>
2026-01-31 00:28:21 +0530

    Add src/components/LearningView.jsx

---
+++ src/components/LearningView.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TopicInput from "./TopicInput.jsx";
+import HelpPopup from "./HelpPopup.jsx";
+
+function LearningView() {
+  return (
+    <div>
+      <h1>LearningView placeholder</h1>
+      <TopicInput />
+      <HelpPopup />
+    </div>
+  );
+}
+
+export default LearningView;
